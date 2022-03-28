03bd0a9e6cce15f1e2a417b31c1197924c151851
Edu58 <edumuriithi58@gmail.com>
2022-03-28 10:42:27 +0300

    fixes upload feat

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -4,7 +4,8 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Upload } from '../models/upload.model';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/storage'
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/compat/storage';
+import { finalize } from 'rxjs';
 
 @Injectable( {
   providedIn: 'root'
@@ -13,37 +14,27 @@ export class UploadService {
 
   private basePath: string = '/uploads'
 
-  constructor ( private AFire: AngularFireModule, private db: AngularFireDatabase ) { }
+  constructor ( private AFire: AngularFireModule, private storage: AngularFireStorage, private db: AngularFireDatabase ) { }
   
   uploadFile ( upload: Upload ): void {
-    const storageRef = firebase.storage().ref()
-    const uploadTask = storageRef.child( `${ this.basePath }/${ upload }` ).put(upload.file)
 
-    //monitor task as it is uploaded
-    uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED ,
-      // 3 Observers
-      // 1. Allow to monitor upload progress
-      ( snapshot ): void => {
-        upload.progress = ( uploadTask.snapshot.bytesTransferred / uploadTask.snapshot.totalBytes ) * 100
-
-      },
-
-      //2. check error
-      ( error: Error ): void => {
-        console.log( error )
-        alert(error)
-      },
-
-      // Success Observer
-      (): void => {
-        upload.url = uploadTask.snapshot.ref.getDownloadURL()
-        upload.name = upload.file.name
-        this.saveFileData(upload)
-      }
-    )
+    const filePath: string = `${ this.basePath }/${ upload.file.name }`
+    const storageRef: AngularFireStorageReference = this.storage.ref( filePath )
+    const uploadTask: AngularFireUploadTask = this.storage.upload( filePath, upload.file )
+    
+    uploadTask.snapshotChanges().pipe(
+      finalize( (): void => {
+        storageRef.getDownloadURL().subscribe( ( url: string ): void => {
+          upload.name = upload.file.name
+          upload.url = url
+          this.saveFileData( upload );
+        } )
+      } )
+    ).subscribe();
   }
+  
   private saveFileData ( upload: Upload ): void {
-    this.db.list( `${ this.basePath }` ).push( upload )
+    this.db.list(this.basePath).push( upload )
     console.log( `File - ${ upload.name } to : ` + upload.url )
-  }
+ }
 }
